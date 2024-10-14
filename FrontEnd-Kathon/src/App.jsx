173e9cf93db512ed0954b2030f6956663bfb2046
Nalcino <phnalcino@gmail.com>
2024-10-14 16:29:51 -0300

    Update

--- FrontEnd-Kathon/src/App.jsx
+++ FrontEnd-Kathon/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Router from 'express/lib/router'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <h1>Tigrinho</h1>
     </>
   )
 }
