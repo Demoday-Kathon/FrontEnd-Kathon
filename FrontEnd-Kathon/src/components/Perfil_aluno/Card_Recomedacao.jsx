e4e3c6559e4c8466d0a3b5f5cdef36b7415fcbab
Nalcino <phnalcino@gmail.com>
2024-10-16 19:15:49 -0300

    update

--- FrontEnd-Kathon/src/components/Perfil_aluno/Card_Recomedacao.jsx
+++ FrontEnd-Kathon/src/components/Perfil_aluno/Card_Recomedacao.jsx
@@ -1,9 +1,17 @@
 import Styles from './Card_Recomendacao.module.css'
-
+import leticia from '../../assets/imgs/Leticia.png'
 
 function Card_Recomendacao(){
     return(
-        a
+        <div className={Styles.Card}>
+            <div className={Styles.Peril_foto}>
+                <img className={Styles.Leticia} src={leticia} alt="Foto Perfil" />
+                    <h1 className={Styles.Perfil_texto}>Leticia Silva</h1>
+                </div>
+               <div className={Styles.Perfil_texto}>
+                    <p>"Excelente Profissional, sua brilhate habilidade em UI/UX e impressionate.</p>
+            </div>
+            </div>
     )
 }
 
