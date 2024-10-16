d8f6b821d39203e9e8df40b1029743d23fb1dbbd
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-10-16 16:13:01 -0300

    Home Titulo

--- FrontEnd-Kathon/src/pages/home/Home.jsx
+++ FrontEnd-Kathon/src/pages/home/Home.jsx
@@ -3,7 +3,8 @@ import styles from './Home.module.css'
 function Home() {
     return (
         <container className={styles.home}>
-            <h1> Home </h1>
+            <h1 className={styles.titulo}> Onde os jovens <span>Talentos</span> encontram desafios e as empresas encontram o <span>Futuro</span> </h1>
+
         
         </container>
     )
