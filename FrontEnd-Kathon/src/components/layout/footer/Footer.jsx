d0d058c798d6aa9e75272f8ecd06f03a82394f11
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-10-17 00:01:41 -0300

    Footer pronto

--- FrontEnd-Kathon/src/components/layout/footer/Footer.jsx
+++ FrontEnd-Kathon/src/components/layout/footer/Footer.jsx
@@ -6,6 +6,9 @@ import logoCompletaKathon from '../../../assets/imgs/logokathoncompleta.png'
 import ListLink from '../listlink/ListLinks';
 import LinkComponent from '../link/LinkComponent';
 
+import { FaGoogle, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 function Footer() {
     return (
         <footer className={styles.footer}>
@@ -14,9 +17,29 @@ function Footer() {
                     <div className={styles.logoFooter}></div>
                     <p> Desafios que revelam talentos, Soluções que moldam o futuro</p>
 
+                    <ListLink customClass="listLinkRowSocial">
+                        <li><FaGoogle /></li>
+                        <li><FaLinkedin /></li>
+                        <li><FaXTwitter /></li>
+                        <li><FaInstagram /></li>
+                    </ListLink>
+
                 </div>
-                <div className={styles.direitoFooter}>
-                    <p> Desafios que revelam talentos soluções que moldam o futuro</p>
+                <div className={styles.direitoFooter1}>
+                    <ListLink customClass="listLinkCollum">
+                        <h3>Kathon</h3>
+                        <li> <LinkComponent linkTo="/" text="Home" /> </li>
+                        <li> <LinkComponent linkTo="/hackathons" text="Hackathon" /> </li>
+                        <li> <LinkComponent linkTo="/empresas" text="Empresas" /> </li>
+                        <li> <LinkComponent linkTo="/comunidades" text="Comunidades" /> </li>
+                        <li> <LinkComponent linkTo="/sobre" text="Sobre a Kathon" /> </li>
+                    </ListLink>
+
+                    <ListLink customClass="listLinkCollum2">
+                        <h3>Contato</h3>
+                        <li> (11) 2632-4489 </li>
+                        <li> kathon@outlook.com </li>
+                    </ListLink>
                 </div>
             </div>
 
@@ -26,6 +49,8 @@ function Footer() {
                 <div className={styles.esquerdoFooter}>
                     <span> © 2024 Kathon Inc. Todos os direitos reservados.</span>
                 </div>
+
+
                 <div className={styles.direitoFooter}>
                     <ListLink customClass="listLinkRow">
                         <li> Política de Privacidade </li>
