66416d9d0d4976d1c10911d1fd487b699721fea2
Ricardo Ribeiro <ricardoribeirodematos@gmail.com>
2024-10-16 22:23:31 -0300

    update

--- FrontEnd-Kathon/src/pages/hackathonsVisaoGeral/HackathonsVisaoGeral.jsx
+++ FrontEnd-Kathon/src/pages/hackathonsVisaoGeral/HackathonsVisaoGeral.jsx
@@ -8,6 +8,7 @@ function HackathonVisaoGeral() {
         context="Como parte de nossa visão de promover a economia local e garantir sustentabilidade, desenvolvemos um aplicativo que conecta diretamente o Terra Vermelha a fornecedores locais de materiais de construção, móveis e serviços relacionados. Esse aplicativo cria uma plataforma de mercado eficiente, reduzindo custos logísticos e fortalecendo a economia regional, ao mesmo tempo em que assegura uma cadeia de suprimentos mais sustentável e personalizada.
 "
         mentors="Mentores"
+        sponsors="Patrocinadores"
       />
     </>
   );
