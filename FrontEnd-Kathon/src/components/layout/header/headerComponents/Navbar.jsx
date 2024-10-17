44ff7474ccc3b47a498c080452e62eba3881a0b4
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-10-16 22:38:11 -0300

    Arrumando conflitos

--- FrontEnd-Kathon/src/components/layout/header/headerComponents/Navbar.jsx
+++ FrontEnd-Kathon/src/components/layout/header/headerComponents/Navbar.jsx
@@ -1,10 +1,16 @@
 import styles from './Navbar.module.css'
 
-import ListLink from './navbarComponents/ListLinks'
-
+import ListLink from '../../listlink/ListLinks.jsx'
+import LinkComponent from '../../link/LinkComponent.jsx';
 function Navbar() {
     return (
-        <ListLink />
+        <ListLink customClass="listLinks">
+            <li> <LinkComponent linkTo="/" text="Home"/> </li>
+            <li> <LinkComponent linkTo="/hackathons" text="Hackathon"/> </li>
+            <li> <LinkComponent linkTo="/empresas" text="Empresas"/> </li>
+            <li> <LinkComponent linkTo="/comunidades" text="Comunidades"/> </li>
+            <li> <LinkComponent linkTo="/sobre" text="Sobre a Kathon"/> </li>
+        </ListLink>
     )
 }
 
--- FrontEnd-Kathon/src/pages/home/Home.jsx
+++ FrontEnd-Kathon/src/pages/home/Home.jsx
@@ -1,17 +1,19 @@
 import styles from './Home.module.css'
 import Header from '../../components/layout/header/Header'
+import Footer from '../../components/layout/footer/Footer'
 
 function Home() {
     return (
-        <>
-            <Header />
+        <div className={styles.home}>
+        <Header />
         <container className={styles.homeTitulo}>
             <h1 className={styles.homeTitulo}> Onde os jovens <span>Talentos</span> encontram desafios e as empresas encontram o <span>Futuro</span> </h1>
             <p  className={styles.descHome}> Conectamos seu negócio com nossos jovens de escolas públicas, através de Hackathons. Você traz o desafio e os jovens criam a melhor solução criativa e tecnológica.</p>
 
         
         </container>
-        </>
+        <Footer />
+        </div>
     )
 }
 
