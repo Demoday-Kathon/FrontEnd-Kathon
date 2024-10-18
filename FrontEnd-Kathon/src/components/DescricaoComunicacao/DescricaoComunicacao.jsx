90b3f6f5ef925d67232344f73e9db1a11d0c2ae4
Gustavo de Sousa Lima <gustavdesousalima@gmail.com>
2024-10-18 15:39:37 -0300

    Update

---
+++ FrontEnd-Kathon/src/components/DescricaoComunicacao/DescricaoComunicacao.jsx
@@ -0,0 +1,9 @@
+import Styles from './DescricaoComunicacao.module.css';
+
+function DescricaoComunicacao() {
+    return (
+        <div className={Styles.containerDescricaoComunicacao}>
+           <h1>etste</h1>
+        </div>
+    )
+}
