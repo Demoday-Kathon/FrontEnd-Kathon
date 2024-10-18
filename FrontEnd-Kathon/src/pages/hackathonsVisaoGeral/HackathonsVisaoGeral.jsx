77a4a44bfe15e11843aef14c71138b886ce409bb
Ricardo Ribeiro <ricardoribeirodematos@gmail.com>
2024-10-17 22:27:19 -0300

    update

--- FrontEnd-Kathon/src/pages/hackathonsVisaoGeral/HackathonsVisaoGeral.jsx
+++ FrontEnd-Kathon/src/pages/hackathonsVisaoGeral/HackathonsVisaoGeral.jsx
@@ -1,17 +1,24 @@
 import CardInscricao from "../../components/cardInscricao/CardInscricao";
 import CardPreto from "../../components/layout/cardPreto/CardPreto";
+import Bann from "../../components/Banner/Bann";
+import Styles from "../hackathonsVisaoGeral/HackathonVisaoGeral.module.css";
 
 function HackathonVisaoGeral() {
   return (
     <>
-      <CardInscricao />
-      <CardPreto
-        title="Rede ou Aplicativo para Conectar Fornecedores Locais"
-        context="Como parte de nossa visão de promover a economia local e garantir sustentabilidade, desenvolvemos um aplicativo que conecta diretamente o Terra Vermelha a fornecedores locais de materiais de construção, móveis e serviços relacionados. Esse aplicativo cria uma plataforma de mercado eficiente, reduzindo custos logísticos e fortalecendo a economia regional, ao mesmo tempo em que assegura uma cadeia de suprimentos mais sustentável e personalizada.
+      <div className={Styles.containerprincipal}>
+        <Bann />
+        <CardInscricao />
+      </div>
+      <div className={Styles.containersubprincipal}>
+        <CardPreto
+          title="Rede ou Aplicativo para Conectar Fornecedores Locais"
+          context="Como parte de nossa visão de promover a economia local e garantir sustentabilidade, desenvolvemos um aplicativo que conecta diretamente o Terra Vermelha a fornecedores locais de materiais de construção, móveis e serviços relacionados. Esse aplicativo cria uma plataforma de mercado eficiente, reduzindo custos logísticos e fortalecendo a economia regional, ao mesmo tempo em que assegura uma cadeia de suprimentos mais sustentável e personalizada.
 "
-        mentors="Mentores"
-        sponsors="Patrocinadores"
-      />
+          mentors="Mentores"
+          sponsors="Patrocinadores"
+        />
+      </div>
     </>
   );
 }
