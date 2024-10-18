1ff45bf5614cf8865efac3c5c2af199ccca47ce6
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-10-18 13:52:29 -0300

    Arrumando NAV LinkComponent

--- FrontEnd-Kathon/src/components/layout/link/LinkComponent.jsx
+++ FrontEnd-Kathon/src/components/layout/link/LinkComponent.jsx
@@ -1,15 +1,24 @@
-import styles from './LinkComponent.module.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './LinkComponent.module.css';
 
-function LinkComponent({ linkTo, text, isHovered, isActive }) {
+function LinkComponent({ text, linkTo, isActive }) {
+    const [hoveredLink, setHoveredLink] = useState(null);
+
+    const handleMouseEnter = () => setHoveredLink(linkTo);  // Quando o mouse entra, muda o estado de hover
+    const handleMouseLeave = () => setHoveredLink(null);    // Quando o mouse sai, reseta o estado de hover
+
     return (
-        <>
-        
-        {text} {isHovered || isActive ? <span>&lt;</span> : null}
-        <Link className={styles.LinkComponent} to={linkTo}>
+        <Link
+            className={styles.LinkComponent}
+            to={linkTo}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            {/* Exibe o "<" */}
+            {hoveredLink === linkTo ? <span>&lt;</span> : null}
+            {text}
         </Link>
-        
-        </>
     );
 }
 
