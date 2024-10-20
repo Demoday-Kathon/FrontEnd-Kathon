20749b4ab4c0087eb8d8e725e7921bd8601aaae9
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-10-19 21:20:15 -0300

    Página escolha do tipo de conta

--- FrontEnd-Kathon/src/pages/cadastroEscolha/CadastroEscolha.jsx
+++ FrontEnd-Kathon/src/pages/cadastroEscolha/CadastroEscolha.jsx
@@ -6,6 +6,10 @@ import CardBrancoBase from '../../components/CardBrancoBase/CardBrancoBase';
 
 import Footer from '../../components/layout/footer/Footer';
 
+import iconeEstudante from  '../../assets/imgs/iconeEstudante.png';
+import iconeEmpresa from   '../../assets/imgs/iconeEmpresa.png';
+
+
 function CadastroEscolha() {
     return (
         <>
@@ -14,12 +18,14 @@ function CadastroEscolha() {
                 <h1 className={styles.tituloCadastroEscola}> Escolha seu tipo de conta </h1>
                 <div className={styles.areaCardsBrancos}>
                     <CardBrancoBase customClass="cardBrancoEscolha">
-                        Testando Card Branco
+                        <img src={iconeEstudante} alt="iconeEstudante" />
+                        Estudante
 
                     </CardBrancoBase>
 
                     <CardBrancoBase customClass="cardBrancoEscolha">
-                        Testando Card Branco
+                        <img src={iconeEmpresa} alt="iconeEstudante" />
+                        Empresa
 
                     </CardBrancoBase>
 
