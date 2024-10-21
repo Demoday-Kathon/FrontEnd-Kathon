7a5e0843f9e5248ed881c64a0024a18f83b656eb
Raphael Pereira de Souza <89276182+raphaelpereirarps03@users.noreply.github.com>
2024-10-20 23:24:04 -0300

    Tela Cadastro Empresa Feito

--- FrontEnd-Kathon/src/components/Form/FormComponents/Input.jsx
+++ FrontEnd-Kathon/src/components/Form/FormComponents/Input.jsx
@@ -1,7 +1,7 @@
 import styles from './Input.module.css'
 
-function Input({text, type}) {
-    return <input className={styles.inputFormsLogin} placeholder={text} type={type}/>
+function Input({text, type, customClass}) {
+    return <input className={`${styles.inputFormsLogin} ${styles[customClass]}`} placeholder={text} type={type}/>
 }
 
 export default Input
