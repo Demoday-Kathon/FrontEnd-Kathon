81a3233f3a3d5eb5179d0ced8f514064a3da182e
lucascarv <lucascarvalhio077@gmail.com>
2024-10-23 16:03:50 -0300

    atualizando

--- FrontEnd-Kathon/src/App.jsx
+++ FrontEnd-Kathon/src/App.jsx
@@ -60,7 +60,6 @@ function App() {
           <Route path="/HackathonsPremios" element={<HackathonsPremios />} />
           <Route path="/Planos" element={<Planos />} />
           <Route path="/dashboardhome" element={<DashBoardHome />} />
-<<<<<<< HEAD
           <Route path="/dashboardrecrutamento" element={<DashboardRecrutamento />} />
           <Route path="/dashboardassistenteia" element={<DashboardAssistenteIA />} />
 
@@ -69,23 +68,12 @@ function App() {
 
           <Route path="/Login" element={<Login />} />
 
-          <Route path="CadastroEscolha" element={<CadastroEscolha />} />
+          <Route path="/CadastroEscolha" element={<CadastroEscolha />} />
 
-          <Route path="CadastroEstudante" element={<CadastroEstudante />} />
-          <Route path="CadastroEmpresa" element={<CadastroEmpresa />} />
-          <Route path="feed" element={<Feed />} />
+          <Route path="/CadastroEstudante" element={<CadastroEstudante />} />
+          <Route path="/CadastroEmpresa" element={<CadastroEmpresa />} />
+          <Route path="/feed" element={<Feed />} />
 
-=======
-          <Route
-            path="/dashboardrecrutamento"
-            element={<DashboardRecrutamento />}
-          />
-          <Route
-            path="/dashboardassistenteia"
-            element={<DashboardAssistenteIA />}
-          />
-          <Route path="/cadastroEmpresa" element={<CadastroEmpresa />} />
->>>>>>> 4269cc8a934672580d4bf6a354b13dea17160d2b
         </Routes>
       </Router>
     </main>
