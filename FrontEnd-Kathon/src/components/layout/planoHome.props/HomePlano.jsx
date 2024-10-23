f6dd785a76ca190cc0e16d5ab4a4e4465457caea
Ricardo Ribeiro <ricardoribeirodematos@gmail.com>
2024-10-23 01:57:54 -0300

    update

--- FrontEnd-Kathon/src/components/layout/planoHome.props/HomePlano.jsx
+++ FrontEnd-Kathon/src/components/layout/planoHome.props/HomePlano.jsx
@@ -1,14 +1,12 @@
-import Style from "../planoHome.props/HomePlano.module.css"
+import Style from "../planoHome.props/HomePlano.module.css";
 
-
-function HomePlano({img, alt, text, customtext}){
-
-    return(
-             <div className={Style.homeplanofilho}>
-                 <img className={Style.planohomeimg} src={img} alt={alt} title={alt} />
-                 <h4 className={`${Style.text} ${Style[customtext]}`} >{text} </h4>
-            </div>
-    )
+function HomePlano({ img, alt, text, customtext }) {
+  return (
+    <div className={Style.homeplanofilho}>
+      <img className={Style.planohomeimg} src={img} alt={alt} title={alt} />
+      <h4 className={`${Style.text} ${Style[customtext]}`}>{text} </h4>
+    </div>
+  );
 }
 
-export default HomePlano
+export default HomePlano;
