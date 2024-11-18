d9bb6d2d08381b8b67b7a26078ae93e134ca3719
lucascarv <lucascarvalhio077@gmail.com>
2024-11-18 02:03:29 -0300

    sessões-dashboard-anali-geral-hackathon

--- src/pages/dashboardVisaoGeral/AnaliseGeral.jsx
+++ src/pages/dashboardVisaoGeral/AnaliseGeral.jsx
@@ -1,11 +1,27 @@
-import './AnaliseGeral.css'
+import Styles from '../dashboardVisaoGeral/AnaliseGeral.module.css'
 import NavBar from '../dashBoardHome/components/NavBar'
 
 function AnaliseGeral(){
     return(
-        <div className="container">
-            <div className="conteudo">
+        <div className={Styles.container}>
+            <NavBar />
+            <div className={Styles.conteudo}>
+                <div className={Styles.ladoEsquerdo}>
+                    <div className={Styles.graficoPizza}>
 
+                    </div>
+                    <div className={Styles.graficoGeral}>
+
+                    </div>
+                </div>
+                <div className={Styles.ladoDireito}>
+                    <div className={Styles.equipes}>
+
+                    </div>
+                    <div className={Styles.projetos}>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
