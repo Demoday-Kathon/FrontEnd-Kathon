b595b82eb9d8e59bbc789d634c14e8f395681108
Gustavo de Sousa Lima <gustavdesousalima@gmail.com>
2024-11-22 12:22:18 -0300

    Updates

--- assets/handlebars-BBTjlHG4.js
+++ assets/handlebars-BBTjlHG4.js
@@ -1,4 +1,4 @@
-import{m as i}from"./editor.main-BAkvZASE.js";import"./index-DaVLFCKT.js";/*!-----------------------------------------------------------------------------
+import{m as i}from"./editor.main-C8CEa1aM.js";import"./index-D14SVdcl.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.52.0(f6dc0eb8fce67e57f6036f4769d92c1666cdf546)
  * Released under the MIT license
