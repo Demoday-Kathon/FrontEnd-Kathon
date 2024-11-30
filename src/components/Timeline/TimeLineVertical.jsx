81046c088eee522a61b49549ca7a1b2e022ecbce
lucascarv <lucascarvalhio077@gmail.com>
2024-11-30 02:10:02 -0300

    update

--- src/components/Timeline/TimeLineVertical.jsx
+++ src/components/Timeline/TimeLineVertical.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import "../../components/Timeline/TimelineVertical.css";
 import iconClock from "../../assets/imgs/icones/clock.png";
 import iconCalendar from "../../assets/imgs/icones/calendar.png";
-import { IconBase } from "react-icons/lib";
+import { motion } from "framer-motion";
+import { fadein } from '../../variants'
 
 const TimelineVertical = () => {
   const [scrollProgress, setScrollProgress] = useState(0);
   const maxScrollHeight = 2140;
-  const delay = 1400;
+  const delay = 900;
 
   useEffect(() => {
     const handleScroll = () => {
@@ -44,99 +45,149 @@ const TimelineVertical = () => {
       <div className="timeline">
         <div className="event">
           <div className="event-content">
-            <h3>Inicio das Inscrições</h3>
-            <div className="icon-content">
-              <img src={iconCalendar} alt="" srcset="" />
-              <p>27 de setembro de 2024</p>
-            </div>
-            <div className="icon-content">
-              <img src={iconClock} alt="" srcset="" />
-              <p>09:00hrs ás 22:00hrs</p>
-            </div>
+           <div className="caixa-info">
+              <h3>Inicio das Inscrições</h3>
+              <div className="icon-content">
+                <img src={iconCalendar} alt="" srcset="" />
+                <p>27 de setembro de 2024</p>
+              </div>
+              <div className="icon-content">
+                <img src={iconClock} alt="" srcset="" />
+                <p>09:00hrs ás 22:00hrs</p>
+              </div>
+           </div>
           </div>
         </div>
 
         <div className="event">
           <div className="event-content">
-            <h3>Confirmação dos times</h3>
-            <div className="icon-content">
-              <img src={iconCalendar} alt="" srcset="" />
-              <p>27 de setembro de 2024</p>
-            </div>
-            <div className="icon-content">
-              <img src={iconClock} alt="" srcset="" />
-              <p>09:00hrs ás 22:00hrs</p>
-            </div>
+            <motion.div 
+            className="caixa-info"
+            variants={fadein("left", 0.9)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            >
+              <h3>Confirmação dos times</h3>
+              <div className="icon-content">
+                <img src={iconCalendar} alt="" srcset="" />
+                <p>27 de setembro de 2024</p>
+              </div>
+              <div className="icon-content">
+                <img src={iconClock} alt="" srcset="" />
+                <p>09:00hrs ás 22:00hrs</p>
+              </div>
+            </motion.div>
           </div>
         </div>
 
         <div className="event">
           <div className="event-content">
-            <h3>Inicio do Hackathon</h3>
-            <div className="icon-content">
-              <img src={iconCalendar} alt="" srcset="" />
-              <p>27 de setembro de 2024</p>
-            </div>
-            <div className="icon-content">
-              <img src={iconClock} alt="" srcset="" />
-              <p>09:00hrs ás 22:00hrs</p>
-            </div>
+            <motion.div 
+            className="caixa-info"
+            variants={fadein("left", 0.9)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            >
+              <h3>Inicio do Hackathon</h3>
+              <div className="icon-content">
+                <img src={iconCalendar} alt="" srcset="" />
+                <p>27 de setembro de 2024</p>
+              </div>
+              <div className="icon-content">
+                <img src={iconClock} alt="" srcset="" />
+                <p>09:00hrs ás 22:00hrs</p>
+              </div>
+            </motion.div>
           </div>
         </div>
 
         <div className="event">
           <div className="event-content">
-            <h3>Liberação do tema</h3>
-            <div className="icon-content">
-              <img src={iconCalendar} alt="" srcset="" />
-              <p>27 de setembro de 2024</p>
-            </div>
-            <div className="icon-content">
-              <img src={iconClock} alt="" srcset="" />
-              <p>09:00hrs ás 22:00hrs</p>
-            </div>
+            <motion.div 
+            className="caixa-info"
+            variants={fadein("left", 0.9)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            >
+              <h3>Liberação do tema</h3>
+              <div className="icon-content">
+                <img src={iconCalendar} alt="" srcset="" />
+                <p>27 de setembro de 2024</p>
+              </div>
+              <div className="icon-content">
+                <img src={iconClock} alt="" srcset="" />
+                <p>09:00hrs ás 22:00hrs</p>
+              </div>
+            </motion.div>
           </div>
         </div>
 
         <div className="event">
           <div className="event-content">
-            <h3>Desenvolvimento</h3>
-            <div className="icon-content">
-              <img src={iconCalendar} alt="" srcset="" />
-              <p>27 de setembro de 2024</p>
-            </div>
-            <div className="icon-content">
-              <img src={iconClock} alt="" srcset="" />
-              <p>09:00hrs ás 22:00hrs</p>
-            </div>
+            <motion.div 
+              className="caixa-info"
+              variants={fadein("left", 0.9)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.9 }}
+            >
+              <h3>Desenvolvimento</h3>
+              <div className="icon-content">
+                <img src={iconCalendar} alt="" srcset="" />
+                <p>27 de setembro de 2024</p>
+              </div>
+              <div className="icon-content">
+                <img src={iconClock} alt="" srcset="" />
+                <p>09:00hrs ás 22:00hrs</p>
+              </div>
+            </motion.div>
           </div>
         </div>
 
         <div className="event">
           <div className="event-content">
-            <h3>Prazo para envio</h3>
-            <div className="icon-content">
-              <img src={iconCalendar} alt="" srcset="" />
-              <p>27 de setembro de 2024</p>
-            </div>
-            <div className="icon-content">
-              <img src={iconClock} alt="" srcset="" />
-              <p>09:00hrs ás 22:00hrs</p>
-            </div>
+            <motion.div 
+            className="caixa-info"
+            variants={fadein("left", 0.9)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            >
+              <h3>Prazo para envio</h3>
+              <div className="icon-content">
+                <img src={iconCalendar} alt="" srcset="" />
+                <p>27 de setembro de 2024</p>
+              </div>
+              <div className="icon-content">
+                <img src={iconClock} alt="" srcset="" />
+                <p>09:00hrs ás 22:00hrs</p>
+              </div>
+            </motion.div>
           </div>
         </div>
 
         <div className="event">
           <div className="event-content">
-            <h3>Resultado</h3>
-            <div className="icon-content">
-              <img src={iconCalendar} alt="" srcset="" />
-              <p>27 de setembro de 2024</p>
-            </div>
-            <div className="icon-content">
-              <img src={iconClock} alt="" srcset="" />
-              <p>09:00hrs ás 22:00hrs</p>
-            </div>
+            <motion.div 
+            className="caixa-info"
+            variants={fadein("left", 0.9)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.9 }}
+            >
+              <h3>Resultado</h3>
+              <div className="icon-content">
+                <img src={iconCalendar} alt="" srcset="" />
+                <p>27 de setembro de 2024</p>
+              </div>
+              <div className="icon-content">
+                <img src={iconClock} alt="" srcset="" />
+                <p>09:00hrs ás 22:00hrs</p>
+              </div>
+            </motion.div>
           </div>
         </div>
       </div>
